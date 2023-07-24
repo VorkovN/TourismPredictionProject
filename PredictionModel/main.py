795edce973474147002cdc0c107f48486b7d19c9
Nikita Vorkov <vorkov711@gmail.com>
2023-07-25 02:43:12 +0300

    first mlPredictor

--- PredictionModel/main.py
+++ PredictionModel/main.py
@@ -1,53 +1,105 @@
 import pika, sys, os
 from threading import Thread
-import time
 import json
+import joblib
+import pandas as pd
 
-def regularMessageCallback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
 
-def handleRegularMessage(channelReceive):
-    channelReceive.basic_consume(queue='server2ml-q1', on_message_callback=regularMessageCallback, auto_ack=True)
-    channelReceive.start_consuming()
+DataMlToServer = {
+    "name": "",
+    "popularity": 0.0,
+}
 
-def sendRegularMessage(channelSend):
-    while(True):
-        data = {
-            "id": 1,
-            "name": "My Name",
-            "description": "This is description about me"
-        }
+DataServerToMl = {
+    "name" : "",
+    "latitude" : 0.0,
+    "longitude" : 0.0,
+    "coeffNearestPopularity" : 0.0,
+    "carAvailability" : False,
+    "busAvailability" : False,
+    "bigCarAvailability" : False,
+    "shipAvailability" : False,
+    "planeAvailability" : False,
+    "theatre" : False,
+    "ethnicCenter" : False,
+    "museum" : False,
+    "childrensTourism" : False,
+    "cityAttractions" : False,
+    "attraction" : False,
+    "culturalCentre" : False,
+    "shipbuilding" : False,
+    "nationalPark" : False,
+    "sanatorium" : False,
+    "lookout" : False,
+    "skiResort" : False,
+}
 
-        message = json.dumps(data)
-        channelSend.basic_publish(exchange='ml2server', routing_key='ml2server-q1', body='Hello World! 3')
-        time.sleep(1)
+class MLPredictor():
+
+    def __init__(self):
+        super().__init__()
+
+        connectionSend = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
+        self.channelSend = connectionSend.channel()
+
+        try:
+            self.channelSend.exchange_declare('ml2server', exchange_type='direct')
+            self.channelSend.queue_declare(queue='ml2server-q1')
+        except Exception:
+            print('ml2server has already declared')
+        self.channelSend.queue_bind(queue='ml2server-q1', exchange='ml2server', routing_key='ml2server-q1')
+
+        connectionReceive = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        self.channelReceive = connectionReceive.channel()
+
+        try:
+            self.channelReceive.exchange_declare('server2ml', exchange_type='direct')
+            self.channelReceive.queue_declare(queue='server2ml-q1')
+        except Exception:
+            print('server2ml has already declared')
+
+        self.channelReceive.basic_consume(queue='server2ml-q1', on_message_callback=self.regularMessageCallback, auto_ack=True)
+        self.channelReceive.start_consuming()
+
+    def regularMessageCallback(self, ch, method, properties, body):
+        dataServerToMl = json.loads(body)
+        print("Received %r" % dataServerToMl)
+
+        paramsVector = {'Широта': [dataServerToMl['latitude']],
+                        'Долгота': [dataServerToMl['longitude']],
+                        'Коэффициент кол-ва запросов в месяц окружающих объектов': [dataServerToMl['coeffNearestPopularity']],
+                        'Доступность автомобиля': [dataServerToMl['carAvailability']],
+                        'Доступность автобуса': [dataServerToMl['busAvailability']],
+                        'Доступность внедорожника': [dataServerToMl['bigCarAvailability']],
+                        'Доступность водныого транспорта': [dataServerToMl['shipAvailability']],
+                        'Доступность самолета': [dataServerToMl['planeAvailability']],
+                        'Театр': [dataServerToMl['theatre']],
+                        'Этнический центр': [dataServerToMl['ethnicCenter']],
+                        'Музей': [dataServerToMl['museum']],
+                        'Детский туризм': [dataServerToMl['childrensTourism']],
+                        'Городские достопримечательности': [dataServerToMl['cityAttractions']],
+                        'Достопримечательность': [dataServerToMl['attraction']],
+                        'Культурный центр': [dataServerToMl['culturalCentre']],
+                        'Судостроение': [dataServerToMl['shipbuilding']],
+                        'Нац. парк': [dataServerToMl['nationalPark']],
+                        'Санаторий': [dataServerToMl['sanatorium']],
+                        'Обзорная площадка': [dataServerToMl['lookout']],
+                        'Горнолыжный курорт': [dataServerToMl['skiResort']]}
+        data = pd.DataFrame(paramsVector)
+        print(data)
+        reg = joblib.load('regression_model.joblib')
+        predictions = reg.predict(data)
+
+        dataMlToServer = DataMlToServer
+        dataMlToServer["name"] = dataServerToMl['name']
+        dataMlToServer["popularity"] = predictions.data.item(0)
+        print(dataMlToServer["popularity"])
+        self.channelSend.basic_publish(exchange='ml2server', routing_key='ml2server-q1', body=json.dumps(dataMlToServer))
+        print("Messagew sent")
+
 
 def main():
-
-    connectionReceive = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-    channelReceive = connectionReceive.channel()
-
-    try:
-        channelReceive.exchange_declare('server2ml', exchange_type='direct')
-        channelReceive.queue_declare(queue='server2ml-q1')
-    except Exception:
-        print('server2ml has already declared')
-
-    recvThread = Thread(target=handleRegularMessage, args=(channelReceive,))
-    recvThread.start()
-
-    connectionSend = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
-    channelSend = connectionSend.channel()
-
-    try:
-        channelSend.exchange_declare('ml2server', exchange_type='direct')
-        channelSend.queue_declare(queue='ml2server-q1')
-    except Exception:
-        print('ml2server has already declared')
-
-    channelSend.queue_bind(queue='ml2server-q1', exchange='ml2server', routing_key='ml2server-q1')
-    sendThread = Thread(target=sendRegularMessage, args=(channelSend,))
-    sendThread.start()
+    mlPredictor = MLPredictor()
 
 if __name__ == '__main__':
     try:
